0f0176e7a4b52d8d7c1a1e8a789ade0c15996cbf
saracen75 <saracen75@gmail.com>
2024-05-31 07:08:13 +0800

    completed tests for DecentralizedStableCoin

--- test/unit/DecentralizedStableCoinTest.t.sol
+++ test/unit/DecentralizedStableCoinTest.t.sol
@@ -39,8 +39,16 @@ contract DecentralizedStableCoinTest is Test {
     ////////////////////////////////////////////////////////////////////
     // Unit tests for transferOwnership()
     ////////////////////////////////////////////////////////////////////
+    function testTransferOwnershipRequestFromNonOwner() external {
+        address REQUESTOR = makeAddr("requestor");
+        address NEWOWNER = makeAddr("newowner");
+        vm.prank(REQUESTOR);
+        vm.expectRevert();
+        coin.transferOwnership(NEWOWNER);
+    }
     function testTransferOwnershipPerformedCorrectly() external {
         address NEWOWNER = makeAddr("new owner");
+        vm.prank(coin.owner());
         coin.transferOwnership(NEWOWNER);
         assert(coin.owner() == NEWOWNER);
     }
@@ -49,37 +57,44 @@ contract DecentralizedStableCoinTest is Test {
     // Unit tests for mint()
     ////////////////////////////////////////////////////////////////////
     function testMintRequestFromNonOwner() external {
+        address REQUESTOR = makeAddr("requestor");
         address USER = makeAddr("user");
         uint256 mintAmount = 1e5;
-        vm.prank(USER);
+        vm.prank(REQUESTOR);
         vm.expectRevert();
         coin.mint(USER,mintAmount);
     }
     function testMintToZeroAddress() external {
         uint256 mintAmount = 1e5;   // 100,000 tokens
+        vm.prank(coin.owner());
         vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__InvalidAddress.selector);
         coin.mint(address(0),mintAmount);
     }
     function testMintAmountZero() external {
         address USER = makeAddr("user");
+        vm.prank(coin.owner());
         vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__AmountMustBeMoreThanZero.selector);
         coin.mint(USER,0);
     }
-    function testTotalSupplyAfterMint() external {
+    function testTotalSupplyAfterMint(uint256 mintAmount) external {
+        mintAmount = bound(mintAmount, 1, type(uint256).max);
         address USER = makeAddr("user");
         uint256 totalSupplyBeforeMint = coin.totalSupply();
-        uint256 mintAmount = 1e9;   // 1 billion tokens
+        //uint256 mintAmount = 1e9;   // 1 billion tokens
         console.log("totalSupplyBeforeMint: ",totalSupplyBeforeMint);
         console.log("mintAmount: ",mintAmount);
+        vm.prank(coin.owner());
         coin.mint(USER,mintAmount);
         assert(coin.totalSupply() == mintAmount);
     }
-    function testBalanceOfUserAfterMint() external {
+    function testBalanceOfUserAfterMint(uint256 mintAmount) external {
+        mintAmount = bound(mintAmount, 1, type(uint256).max);
         address USER = makeAddr("user");
         uint256 balanceOfUserBeforeMint = coin.balanceOf(USER);
-        uint256 mintAmount = 1e9;   // 1 billion tokens
+        //uint256 mintAmount = 1e9;   // 1 billion tokens
         console.log("balanceOfUserBeforeMint: ",balanceOfUserBeforeMint);
         console.log("mintAmount: ",mintAmount);
+        vm.prank(coin.owner());
         coin.mint(USER,mintAmount);
         assert(coin.balanceOf(USER) == mintAmount);
     }
@@ -87,9 +102,9 @@ contract DecentralizedStableCoinTest is Test {
     // Unit tests for burn()
     ////////////////////////////////////////////////////////////////////
     function testBurnRequestFromNonOwner() external {
-        address USER = makeAddr("user");
+        address REQUESTOR = makeAddr("requestor");
         uint256 burnAmount = 1e9;   // 1 billion tokens
-        vm.prank(USER);
+        vm.prank(REQUESTOR);
         vm.expectRevert();
         coin.burn(burnAmount);
     }
@@ -98,18 +113,37 @@ contract DecentralizedStableCoinTest is Test {
         vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__AmountMustBeMoreThanZero.selector);
         coin.burn(0);
     }
-    function testTotalSupplyAfterBurn() external {
-        uint256 initialSupply = 2e9;    // 2 billion tokens
+    function testTotalSupplyAfterBurn(uint256 initialSupply,uint256 burnAmount) external {
+        initialSupply = bound(initialSupply,1,type(uint256).max);
+        burnAmount = bound(burnAmount,1,initialSupply);
+        //uint256 initialSupply = 2e9;    // 2 billion tokens
+        vm.prank(coin.owner());
         coin.mint(coin.owner(),initialSupply);
         uint256 totalSupplyBeforeBurn = coin.totalSupply();
-        uint256 burnAmount = 1000;
+        //uint256 burnAmount = 1000;
         console.log("totalSupplyBeforeBurn: ",totalSupplyBeforeBurn);
         console.log("burnAmount: ",burnAmount);
+        vm.prank(coin.owner());
         coin.burn(burnAmount);
         assert(coin.totalSupply() == (totalSupplyBeforeBurn - burnAmount));
     }
+    function testBalanceOfOwnerAfterBurn(uint256 initialSupply,uint256 burnAmount) external {
+        initialSupply = bound(initialSupply,1,type(uint256).max);
+        burnAmount = bound(burnAmount,1,initialSupply);
+        //uint256 initialSupply = 2e9;    // 2 billion tokens
+        vm.prank(coin.owner());
+        coin.mint(coin.owner(),initialSupply);
+        uint256 balanceOfOwnerBeforeBurn = coin.balanceOf(coin.owner());
+        //uint256 burnAmount = 1000;
+        console.log("balanceOfOwnerBeforeBurn: ",balanceOfOwnerBeforeBurn);
+        console.log("burnAmount: ",burnAmount);
+        vm.prank(coin.owner());
+        coin.burn(burnAmount);
+        assert(coin.balanceOf(coin.owner()) == (balanceOfOwnerBeforeBurn - burnAmount));
+    }
     function testBurnAmountExceedsBalance() external {
         uint256 mintAmount = 1e9;   // 1 billion tokens
+        vm.prank(coin.owner());
         coin.mint(coin.owner(),mintAmount);
         uint256 balanceOfUser = coin.balanceOf(coin.owner());
         console.log("balanceOfUser: ",balanceOfUser);
